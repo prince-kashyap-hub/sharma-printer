69cb61dedcf65be1a92b1303b0ca8ce2978e64c9
Prince kashyap <74972414+prince-kashyap-hub@users.noreply.github.com>
2026-09-16 00:36:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,19 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
-  return {
-    base: '/sharma-printer/',
+export default defineConfig({
+  base: '/sharma-printer/',
 
-    plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
 
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
+  },
 
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
-    },
-  };
+  server: {
+    hmr: process.env.DISABLE_HMR !== 'true',
+    watch: process.env.DISABLE_HMR === 'true' ? null : {},
+  },
 });
